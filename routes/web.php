b26f644adba67f66f9a2fcff44e215efb1dc69e3
belhard-user <ldarik2l@gmail.com>
2017-02-26 20:46:52 +0200

    routing

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return "<a href='/hello-trinity-neo'>say hello</a>";
+});
+
+Route::get('hello-{people}-{name}', function($people, $name){
+    return 'hello ' . ucfirst($people) . ' from ' . ucfirst($name);
+})->where([
+    'people' => '\d',
+    'name' => '[5-8]'
+]);
+
+Route::group(['prefix' => 'admin'], function($r){
+    $r->get('user', function(){return 'users';});
+    $r->get('dashboard', function(){return 'dashboard';});
+    $r->get('money-history', function(){return 'money-history';});
 });
