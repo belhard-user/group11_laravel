cdad458fb6bb767c18d2ef71958fd84475e65bd7
belhard-user <ldarik2l@gmail.com>
2017-03-05 17:47:06 +0200

    tmp commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,20 +11,8 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-class HomeController extends Controller
-{
     public function index()
     {
-        $names = [
-            'Neo',
-            'Morpheus',
-            'Tank',
-            'Trinity',
-            'Dozer',
-            'Smith'
-        ];
-
-        \Debugbar::info($names);
         return view('home');
     }
 }
