138b60f9c5d8884093d0ae8bf27499c3e9f73556
belhard-user <ldarik2l@gmail.com>
2017-03-05 17:27:31 +0200

    add design

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,6 @@
 <?php
 
-Route::get('/', function () {
-    return "<a href='/hello-trinity-neo'>say hello</a>";
-});
+Route::get('/', 'HomeController@index');
+Auth::routes();
 
-Route::get('hello-{people}-{name}', function($people, $name){
-    return 'hello ' . ucfirst($people) . ' from ' . ucfirst($name);
-})->where([
-    'people' => '\d',
-    'name' => '[5-8]'
-]);
 
-Route::group(['prefix' => 'admin'], function($r){
-    $r->get('user', function(){return 'users';});
-    $r->get('dashboard', function(){return 'dashboard';});
-    $r->get('money-history', function(){return 'money-history';});
-});
