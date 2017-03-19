4859ddcee6ddfad320452872a14f8e6d09d1b2f1
belhard-user <ldarik2l@gmail.com>
2017-03-19 17:35:25 +0200

    validation

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    @include('part.errors')
     {!! Form::model($article, ['route' => ['article.update', 'slug' => $article->slug], 'method' => 'put' ]) !!}
     @include('articles.form_part', ['btnText' => 'Обновить'])
     {!! Form::close() !!}
